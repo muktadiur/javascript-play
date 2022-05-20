984625a7a928a543e41855312457bfd633ef0a18
Muktadiur Rahman <muktadiur@gmail.com>
2022-05-20 15:54:04 +0600

    prettier added

--- xhr.js
+++ xhr.js
@@ -1,38 +1,35 @@
-
-
 function http(method, url, data) {
     const promise = new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
         xhr.open(method, url);
-        xhr.responseType = 'application/json';
+        xhr.responseType = "application/json";
         xhr.send(data);
 
-        xhr.onload = function() {
+        xhr.onload = function () {
             if (xhr.status >= 400) {
-                reject('Something went wrong.');
+                reject("Something went wrong.");
             } else {
                 resolve(xhr.response);
             }
-        }
+        };
 
-        xhr.onerror = function() {
+        xhr.onerror = function () {
             reject(xhr.response);
-        }
+        };
     });
     return promise;
 }
 
-http('GET', 'https://jsonplaceholder.typicode.com/todos/1')
+http("GET", "https://jsonplaceholder.typicode.com/todos/1")
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
 
-
 data = JSON.stringify({
-    title: 'foo',
-    body: 'bar',
+    title: "foo",
+    body: "bar",
     userId: 1,
-  })
+});
 
-http('POST', 'https://jsonplaceholder.typicode.com/posts', data)
+http("POST", "https://jsonplaceholder.typicode.com/posts", data)
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
